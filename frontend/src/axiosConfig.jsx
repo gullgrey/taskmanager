93f31aa27840fb758b071015c7a45e93242e9ed0
gullgrey <thomas.cleal@connect.qut.edu.au>
2025-03-19 13:09:11 +1000

    Tutorial 3.3

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001", // local
-  baseURL: "http://3.104.37.202", // live
+  //baseURL: "http://localhost:5001", // local
+  baseURL: "http://3.104.37.202:5001", // live
   headers: { "Content-Type": "application/json" },
 });
 
